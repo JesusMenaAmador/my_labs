21b76e2dfe7d36f9d58a968574291f63ff39640c
Jesus <jesus.menaamador@ucr.ac.cr>
2025-04-13 16:07:23 -0600

    Se añade carpeta Models

---
+++ Laboratorio4_B54291/backend-lab-b54291/Models/PaisModel.cs
@@ -0,0 +1,13 @@
+﻿namespace backend_lab_b54291.Models
+{
+    public class PaisModel
+    {
+            public int Id { get; set; }
+
+            public string Nombre { get; set; }
+
+            public string Contiene { get; set; }
+
+            public string Idioma { get; set; }
+    }
+}
