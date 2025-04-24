17a183597aca709edc0904cb909b86b3c4ab40c7
Jesus <jesus.menaamador@ucr.ac.cr>
2025-04-24 16:39:01 -0600

    Laboratorio #5, se añade el formulario de agregar paises

--- Laboratorio3_B54291/frontend-lab/src/components/ListaPaises.vue
+++ Laboratorio3_B54291/frontend-lab/src/components/ListaPaises.vue
@@ -1,25 +1,35 @@
 <template>
     <div class="container mt-5">
         <h1 class="display-4 text-center">Lista de países</h1>
-        <table class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth"
-            >
+        <div class="row justify-content-end">
+            <div class="col-2">
+                <a href="/pais">
+                    <button type="button" class="btn btn-outline-secondary float-right">
+                        Agregar país
+                    </button>
+                </a>
+            </div>
+        </div>
+        <table 
+            class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth"
+        >
             <thead>
                 <tr>
-                <th>Nombre</th>
-                <th>Continente</th>
-                <th>Idioma</th>
-                <th>Acciones</th>
+                    <th>Nombre</th>
+                    <th>Continente</th>
+                    <th>Idioma</th>
+                    <th>Acciones</th>
                 </tr>
             </thead>
             <tbody>
                 <tr v-for="(pais, index) of paises" :key="index">
-                <td>{{ pais.nombre }}</td>
-                <td>{{ pais.continente }}</td>
-                <td>{{ pais.Idioma }}</td>
-                <td>
-                    <button class="btn btn-secondary btn-sm">Editar</button>
-                    <button class="btn btn-danger btn-sm"  v-on:click="eliminarPais(index)">Eliminar</button> <!--@click = v-on:click-->
-                </td>
+                    <td>{{ pais.nombre }}</td>
+                    <td>{{ pais.continente }}</td>
+                    <td>{{ pais.Idioma }}</td>
+                    <td>
+                        <button class="btn btn-secondary btn-sm">Editar</button>
+                        <button class="btn btn-danger btn-sm"  v-on:click="eliminarPais(index)">Eliminar</button> <!--@click = v-on:click-->
+                    </td>
                 </tr>
             </tbody>
         </table>
