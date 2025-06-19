3084f3b4953cbaa3ac303817e3796a16205761ba
Jesus <jesus.menaamador@ucr.ac.cr>
2025-06-19 00:15:49 -0600

    Laboratorio #6, Se añade el video de la ejecución de la prueba del entregable 2

---
+++ Laboratorio4_B54291/UIAutomationTests/Selenium.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using NUnit.Framework;
+
+namespace UIAutomationTests
+{
+    [TestFixture]
+    public class Selenium
+    {
+        IWebDriver _driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            _driver = new FirefoxDriver();
+        }
+
+        [Test]
+        public void Enter_To_List_Of_Countries_Test()
+        {
+            var URL = "http://localhost:8080/";
+
+            _driver.Manage().Window.Maximize();
+            _driver.Navigate().GoToUrl(URL);
+
+            Assert.That(_driver.Title, Is.Not.Empty, "The page title should not be empty after navigating to the URL.");
+            }
+    }
+}
